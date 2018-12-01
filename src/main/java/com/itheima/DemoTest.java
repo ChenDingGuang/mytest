85dcd89bbb9baae54511981e8c336b1d7bdde488
admin <948923345@qq.com>
2018-12-01 11:15:24 +0800

    在idea内需修改了

---
+++ src/main/java/com/itheima/DemoTest.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("-----------------------");
+        System.out.println("-----------------------");
+        System.out.println("-----------------------");
+        System.out.println("----------1111-------------");
+        System.out.println("222222222222222222222222");
+    }
+}
